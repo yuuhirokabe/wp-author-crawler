4cade24dad766b88491f6838497bd124d34128d1
unknown <alekss273@inbox.lv>
2020-02-19 09:27:18 +0200

    Created v1.0

---
+++ wp-crawler.py
@@ -0,0 +1,57 @@
+from bs4 import BeautifulSoup as bs4
+import requests
+import sys
+import random
+
+
+# Randomizing User-Agent's
+def user_agent():
+    agents = [
+        'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.10; rv:69.0) Gecko/20100101 Firefox/69.0',
+        'Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3833.101 Safari/537.36',
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3833.141 Safari/537.36',
+        'Mozilla/5.0 (X11; Gentoo; rv:69.0) Gecko/20100101 Firefox/69.0',
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.143 Safari/537.36',
+        'Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0'
+    ]
+    i = random.randrange(0, len(agents))
+    return agents[i]
+
+print(user_agent())
+
+# Requesting user to input website url to crawl
+try:
+    url = input('Type website url to crawl: ')
+except KeyboardInterrupt:
+    sys.exit()
+
+# Requesting user to input how many users want to crawl
+try:
+    count = int(input('How many users you want to crawl? '))
+except KeyboardInterrupt:
+    sys.exit()
+except ValueError:
+    print('[-] Please type an integer!')
+    sys.exit()
+
+
+try:
+    
+    for i in range(count):
+        i+= 1
+        headers = {
+            'User-Agent': user_agent()
+        }
+        req_url = url + '/?author=%d' % (i)
+        r = requests.get(req_url, headers=headers)
+        soup = bs4(r.text, 'html.parser')
+        idenf= soup.findAll(text='Oops! That page can’t be found.')
+        print(idenf)
+except KeyboardInterrupt:
+    sys.exit()
+except requests.exceptions.MissingSchema:
+    print('[-] Please type url with http(s)!')
+    sys.exit()
+except:
+    print('[-] Entered URL is invalid, please type valid url...')
+    sys.exit()
